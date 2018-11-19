159a3f66a95884cf04e118dd719d6a3b335492e6
Ravichandra P <ravichandra.p@outlook.com>
2018-11-19 15:23:48 +0530

    Update OddEventIfElse.java

--- src/com/org/hackerrank/OddEventIfElse.java
+++ src/com/org/hackerrank/OddEventIfElse.java
@@ -1,12 +1,6 @@
 package com.org.hackerrank;
 
-import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.regex.*;
+import java.util.Scanner;
 
 public class OddEventIfElse {
 

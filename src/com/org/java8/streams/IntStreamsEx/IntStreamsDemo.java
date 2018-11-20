8923ddebc5934f86f4a02fd09a1f3b118284cc1b
Ravichandra P <ravichandra.p@outlook.com>
2018-11-20 20:13:35 +0530

    Update IntStreamsDemo.java

--- src/com/org/java8/streams/IntStreamsEx/IntStreamsDemo.java
+++ src/com/org/java8/streams/IntStreamsEx/IntStreamsDemo.java
@@ -2,30 +2,27 @@ package com.org.java8.streams.IntStreamsEx;
 
 import java.util.Map;
 import java.util.function.Function;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-
-import javax.swing.text.html.parser.Entity;
 
 public class IntStreamsDemo {
 
 	public static void main(String[] args) {
-		System.out.println(Function.identity());
-		funMap();
+		
+		skipDemo();
 
 	}
 	
-	private static void fun1() {
+	private static void ofDemo() {
 		IntStream.of(1,2,3,4,5).forEach(System.out::println);
 	}
 
-	private static void fun2() {
+	private static void rangeClosedDemo() {
 		//IntStream.range(1,5).forEach(System.out::println);
 		IntStream.rangeClosed(1,5).forEach(System.out::println);
 	}
 	
-	private static void fun3() {
+	private static void iterateDemo() {
 		
 		IntStream.iterate(1,i->funS(i)).limit(10).forEach(System.out::println);
 	}
@@ -43,4 +40,10 @@ public class IntStreamsDemo {
 			System.out.printf("%d, %d\n", m.getKey(), m.getValue());
 		}
 	}
+	
+	static void skipDemo() {
+		IntStream intStream = IntStream.range(0, 10);
+		intStream.skip(3).forEach(System.out::println);
+	}
+	
 }
